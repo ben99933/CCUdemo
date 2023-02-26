56cc53e001df2bd04f4ac427215bc3ca9dd00c97
ben99933 <yangben999@gmail.com>
2023-02-26 21:13:18 +0800

    update

--- routes/about.js
+++ routes/about.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/about', function(req, res) {
+router.get("/", function(req, res) {
   try{
-    console.log(req);
-    res.render('about');
+    res.render("about");
   }catch(err){
     console.log(err);
   }
