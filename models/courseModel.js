741e953058cee97652f472ea05d18f660e88c638
ben99933 <yangben999@gmail.com>
2023-02-28 14:24:14 +0800

    搜尋提示

---
+++ models/courseModel.js
@@ -0,0 +1,24 @@
+const { resolve } = require('path');
+const database = require('../config/database');
+
+class Course{
+
+}
+
+const model = {
+    async getCourses(class_name){
+        let str = `SELECT * FROM \`course\` where \`class_name\` like '${class_name}%' limit 5;`;
+        return new Promise((resolve, reject) => {
+            database.query(str, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    //console.log(result);
+                    resolve(result);
+                }
+            });
+        });
+    }
+}
+
+module.exports = model;
--- routes/searchCourse.js
+++ routes/searchCourse.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var controller = require('../controllers/courseSearchController');
 
-router.get('/', function(req, res) {
+router.get('/', async function(req, res) {
   const keyword = req.query.keyword;
   //keyword可能還要經過處理，例如去除空白、轉小寫等等
+  //keyword = keyword.trim().toLowerCase();
 
-
-  //=============這裡要用到資料庫用keyword去搜尋==========
-  var list = ["test1", "test2", "test3"];
-  var result = list.filter(item=>item.includes(keyword));
+  //=============讓controller幫我們查==========
+  var array = await controller.searchCourse(keyword);
+  array = array.map((item)=>{
+    let id = item.id;
+    let class_name =  item.class_name;
+    let teacher = item.teacher;
+    let class_time =  item.class_time;
+    let class_room = item.class_room;
+    return `[${id}] ${class_name}, ${teacher}, ${class_time}, ${class_room}`;
+  });
+  var result = array.filter(item=>item.includes(keyword));
   //====================================================
 
   res.send(result);
