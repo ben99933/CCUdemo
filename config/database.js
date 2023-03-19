8f48767a42da283af292cce7e33fb662f8f8dd60
ben99933 <yangben999@gmail.com>
2023-03-19 20:18:51 +0800

    search course

--- config/database.js
+++ config/database.js
@@ -2,7 +2,12 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const connection = new Pool({
-    connectionString: process.env.DATABASE_URL
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    ssl: true // 加上這個屬性
 });
 
 module.exports = connection;
