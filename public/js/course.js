c693bfb5e9362317c04d7a9061291f81458aca82
as6325400 <as6325400@gmail.com>
2023-03-24 13:16:12 +0800

    modify origin credit bug

--- public/js/course.js
+++ public/js/course.js
@@ -333,7 +333,6 @@ function search(){
                 xhr.send();
                 xhr.onload = ()=>{
                     let data = xhr.response;
-                    // console.log(data)
                     for(var i = 0; i < data.length; i++){
                         if(data[i]==undefined)continue;
                         // 以在li上面顯示的字串為key，將資料存入dict
@@ -369,6 +368,8 @@ function search(){
                             listBox.innerHTML = "";
                             searchBox.value = "";
                             // init();
+                            // 為了應付展開列表打不開的問題，所以在這裡重新整理頁面，但實在是太慢了，所以先註解掉
+                            // location.reload();
                         });
                     }
                     if(data.length > 5){
