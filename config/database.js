94b897be506ecdb3951edc4d3c1cd443afc24f22
ben99933 <yangben999@gmail.com>
2023-03-24 21:29:24 +0800

    database bug fix

--- config/database.js
+++ config/database.js
@@ -1,13 +1,11 @@
-var mysql = require('../config/mysqlDB');
-var postgre = require('../config/postgreSQL');
 
 var connection = null;
 if(process.env.USING_DATABASE== "postgre"){
   console.log("using postgre");
-  connection = postgre;
+  connection = require('../config/postgreSQL');
 }else if(process.env.USING_DATABASE== "mysql"){
   console.log("using mysql");
-  connection = mysql;
+  connection = require('../config/mysqlDB');
 }else{
   console.log("please choose a database!!");
   exit();
