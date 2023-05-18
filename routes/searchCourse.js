f89593d14d992228b11da2b8aa92a2e3b6f3a1da
ben99933 <yangben999@gmail.com>
2023-05-18 21:41:32 +0800

    fix search bugs

--- routes/searchCourse.js
+++ routes/searchCourse.js
@@ -7,10 +7,13 @@ router.get('/', async function(req, res) {
   const keyword = req.query.keyword;
   //keyword可能還要經過處理，例如去除空白、轉小寫等等
   //keyword = keyword.trim().toLowerCase();
-  console.log(`keyword=${keyword}`);
-  //if(regex.checkChineseEnglishNum(keyword)==false)res.send(null);
+  
   //=============讓controller幫我們查==========
-  var array = await controller.searchCourses(keyword);
+  var array = null;
+  if(regex.checkChineseEnglishNum(keyword)){
+    array = await controller.searchCourses(keyword);
+  }
+  
   //console.log(array);
   res.send(array);
 });
